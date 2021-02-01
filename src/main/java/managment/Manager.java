314a2c26280480e7c3dcc6031a7234076d1ee986
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-02-01 16:22:49 +0000

    Manager subclass tested methods passing

--- src/main/java/managment/Manager.java
+++ src/main/java/managment/Manager.java
@@ -1,2 +1,17 @@
-package managment;public class Manager {
+package managment;
+
+import staff.Employee;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String niNumber, double salary, String deptName){
+        super(name, niNumber, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName(){
+        return deptName;
+    }
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ManagerTest {
+import managment.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Mitch", "NI223306A", 41437, "IT");
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        manager.raiseSalary(3500);
+        assertEquals(44937, manager.getSalary(), 0.01);
+    }
+
+    public void canPayBonus(){
+        assertEquals(500, manager.payBonus(), 0.01);
+    }
+
 }
